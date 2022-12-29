3b4bd446d52924c45c3531f08f9f8ff2d96cab42
Kedir Ahmed <kedir10mohamed45@gmail.com>
2022-12-29 18:59:00 +0000

    task 2 fixed

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -9,23 +9,11 @@ exec {'update':
 exec {'install Nginx':
   provider => shell,
   command  => 'sudo apt-get -y install nginx'
-  before   => Exec['hello world']
-}
-exec {'hello world!':
-  command  => 'echo "Hello World!" > /var/www/html/index.html',
-}
-exec {'custom 404':
-  command  => 'echo "Ceci n\'est pas une page" > /var/www/html/c_404.html',
-}
-exec {'custom_404 cnf':
-  command  => 'sed -i "/redirect_me/a \\\terror_page 404 /c_404.html;" /etc/nginx/sites-available/default',
-}
-exec {'redirect me':
-  command  => 'sed -i "/server_name _;/ a\\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+  before   => Exec['add_header']
 }
 exec {'add_header':
-  environment => ["HOST=${hostname}"],
-  command     => 'sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOSTNAME\";/" /etc/nginx/nginx.conf'
+
   before      => Exec['restart Nginx'],
 }
 exec {'restart Nginx':
