238a10253cd0e6d74202612f9233efde7a3a8e42
Kedir Ahmed <kedir10mohamed45@gmail.com>
2022-12-29 18:37:39 +0000

    task 2 fixed

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -9,9 +9,24 @@ exec {'update':
 exec {'install Nginx':
   provider => shell,
   command  => 'sudo apt-get -y install nginx'
-  before   => Exec['add_header']
+  before   => Exec['hello world']
 }
-
+exec {'hello world!':
+  provider => shell,
+  command  => 'echo "Hello World!" > /var/www/html/index.html',
+}
+exec {'custom 404':
+  provider => shell,
+  command  => 'echo "Ceci n\'est pas une page" > /var/www/html/c_404.html',
+}
+exec {'custom_404 cnf':
+  provider => shell,
+  command  => 'sed -i "/redirect_me/a \\\terror_page 404 /c_404.html;" /etc/nginx/sites-available/default',
+}
+exec {'redirect me':
+  provider => shell,
+  command  => 'sed -i "/server_name _;/ a\\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+}
 exec {'add_header':
   provider    => shell,
   environment => ["HOST=${hostname}"],
